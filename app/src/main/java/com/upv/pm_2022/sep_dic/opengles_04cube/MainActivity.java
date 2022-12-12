bdc8938e787380bf94242296103d3c719922315a
infante1810 <2010008@upv.edu.mx>
2022-12-12 09:55:06 -0600

    first commit

---
+++ app/src/main/java/com/upv/pm_2022/sep_dic/opengles_04cube/MainActivity.java
@@ -0,0 +1,32 @@
+package com.upv.pm_2022.sep_dic.opengles_04cube;
+
+/*
+Demo original:
+https://github.com/huntto/android-opengles-2.0
+
+Actualización de Librerias, reestructurarion de proyecto y modificaciones menores:
+Dr. Marco Aurelio Nuño Maganda (Noviembre 2021)
+
+ */
+
+import android.opengl.GLSurfaceView;
+
+import androidx.annotation.NonNull;
+
+
+public class MainActivity extends BaseActivity {
+
+    @NonNull
+    @Override
+    protected GLSurfaceView.Renderer newRenderer() {
+        ShapeRenderer shapeRenderer = new ShapeRenderer(new ShapeRenderer.InitGLCallback() {
+            @Override
+            public void onInitGL() {
+                TexturedCube.init(MainActivity.this);
+            }
+        });
+
+        shapeRenderer.addShape(new TexturedCube());
+        return shapeRenderer;
+    }
+}
---
+++ app/src/main/java/com/upv/pm_2022/sep_dic/opengles_04cube/Shape.java
@@ -0,0 +1,5 @@
+package com.upv.pm_2022.sep_dic.opengles_04cube;
+
+public interface Shape {
+    void draw(float[] viewProjectionMatrix);
+}
